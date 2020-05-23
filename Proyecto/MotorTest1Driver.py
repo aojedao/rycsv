108838c6d3e637c6b1bb7b7c1c5c8019f4c42af2
Alejandro Ojeda <aojedao@unal.edu.co>
2020-05-23 14:02:40 +0000

    Update MotorTest1Driver.py

--- Proyecto/MotorTest1Driver.py
+++ Proyecto/MotorTest1Driver.py
@@ -3,51 +3,36 @@ from time import sleep
 
 in1 = 24
 in2 = 23
-en1 = 25
-in3 =17
-in4 = 27
-en2 = 22
+en = 25
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en1,GPIO.OUT)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-p=GPIO.PWM(en1,1000)
-p=GPIO.PWM(en2,1000)
+p=GPIO.PWM(en,1000)
 p.start(25)
 print("\n")
-print("The default speed & direction of motors is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit tr-turn right tl-turn left")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
 while(1):
 
-    x=raw_input()
+    x=input()
     
     if x=='r':
         print("run")
         if(temp1==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
-         
-         GPIO.output(in3,GPIO.HIGH)
-         GPIO.output(in4,GPIO.LOW)
          print("forward")
          x='z'
         else:
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.HIGH)
-         
-         GPIO.output(in3,GPIO.LOW)
-         GPIO.output(in4,GPIO.HIGH)
          print("backward")
          x='z'
 
@@ -56,18 +41,12 @@ while(1):
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
-        
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
         x='z'
 
     elif x=='f':
         print("forward")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
-        
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in4,GPIO.LOW)
         temp1=1
         x='z'
 
@@ -75,9 +54,6 @@ while(1):
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
-        
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.HIGH)
         temp1=0
         x='z'
 
@@ -104,3 +80,4 @@ while(1):
     else:
         print("<<<  wrong data  >>>")
         print("please enter the defined data to continue.....")
+
